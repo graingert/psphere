325cefff689a44b04cc29da973ac8847a07a5085
Thomas Grainger <tagrain@gmail.com>
2019-02-08 16:34:35 +0000

    Back to development: 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ history = open('CHANGES.rst').read().replace('.. :changelog:', '')
 
 setup(
     name="psphere2",
-    version='0.0.0',
+    version='0.0.1.dev0',
     description="vSphere SDK for Python",
     long_description=readme + '\n\n' + history,
     author="Jonathan Kinred",
